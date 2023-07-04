a111e4a396d0ee9b5857cfdd737eb5c5a0fc2932
Cameron Robinson <cameron@ckrobinson.net>
2023-07-04 15:03:40 -0700

    Added item details view file.

---
+++ DealsApp/Screens/ItemDetails/ItemDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetailsView.swift
+//  DealsApp
+//
+//  Created by Cameron Robinson on 7/4/23.
+//
+
+import SwiftUI
+
+struct ItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetailsView()
+    }
+}
