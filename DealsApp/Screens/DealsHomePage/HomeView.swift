4b107a25d5f180ebdf28186201f25eef5e2555c1
Cameron Robinson <cameron@ckrobinson.net>
2023-07-04 09:55:06 -0700

    Added deal cards to display basic info.

--- DealsApp/Screens/DealsHomePage/HomeView.swift
+++ DealsApp/Screens/DealsHomePage/HomeView.swift
@@ -9,14 +9,75 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @StateObject var viewModel: HomeViewModel = HomeViewModel();
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+
+            ForEach(viewModel.deals) { deal in
+                
+                DealCard(deal: deal)
+            }
         }
+    }
+}
+
+struct DealCard: View {
+    
+    let deal: DealData;
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            /// https://swiftwithmajid.com/2021/07/07/mastering-asyncimage-in-swiftui/
+            AsyncImage(url: deal.product.imageUrl) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                ProgressView()
+                    .frame(maxWidth: .infinity, minHeight: 100)
+                    .padding()
+            }
+            .cornerRadius(15)
+            .padding()
+            
+            Text("\(deal.rawData.title)")
+                .font(.body.weight(.bold))
+                .padding(.horizontal)
+            
+            Text(deal.priceString)
+                .font(.footnote)
+                .padding(.horizontal)
+            
+            Divider()
+                .padding(.horizontal)
+            
+            Text(deal.rawData.description)
+                .font(.caption)
+                .padding(.horizontal)
+                .padding(.vertical)
+            
+            HStack {
+                HStack {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("\(deal.rawData.likes.count)")
+                }
+                .cornerRadius(10)
+                
+                Divider()
+                
+                HStack {
+                    Image(systemName: "hand.thumbsdown.fill")
+                    Text("\(deal.rawData.dislikes.count)")
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .background(.white)
+        .cornerRadius(15)
         .padding()
+        .shadow(radius: 4, x: 3, y: 3)
     }
 }
 
