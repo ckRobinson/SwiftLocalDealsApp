aff49d0abf96575b54dc3ccf38712f808e72b2c5
Cameron Robinson <cameron@ckrobinson.net>
2023-07-04 09:53:50 -0700

    Updated view model to load data.

--- DealsApp/Screens/DealsHomePage/HomeViewModel.swift
+++ DealsApp/Screens/DealsHomePage/HomeViewModel.swift
@@ -9,4 +9,26 @@ import Foundation
 
 class HomeViewModel: ObservableObject {
     // Call the fetch call using DealsService
+    @Published var deals: [DealData] = [];
+    let dealsService = DealsService();
+    init() {
+        self.loadDeals()
+    }
+    
+    func loadDeals() {
+        do {
+            let rawDealData = try dealsService.fetchDeals();
+            self.deals = rawDealData.map({ item in
+                DealData(rawData: item);
+            })
+        }
+        catch {
+            if let error = error as? APIError {
+                print(error);
+            }
+            else {
+                print(error.localizedDescription);
+            }
+        }
+    }
 }
