5f54a686c8d6de40432bd3dd22ae473a5d2672cc
Cameron Robinson <cameron@ckrobinson.net>
2023-07-04 09:53:35 -0700

    Added API data model.

---
+++ DealsApp/Screens/Model/DealAPIModel.swift
@@ -0,0 +1,92 @@
+//
+//  DealsModel.swift
+//  DealsApp
+//
+//  Created by Cameron Robinson on 7/3/23.
+//
+
+import Foundation
+
+struct DealsResponse: Codable {
+    let data: DealsResponseData
+}
+
+struct DealsResponseData: Codable {
+    let deals: [ApiDealData]
+}
+
+struct ApiDealData: Codable, Identifiable {
+    let id: String;
+    let title: String;
+    let url: String;
+    let price: Int;
+    let description: String;
+    let product: ApiProductData;
+    let createdAt: String;
+    let updatedAt: String;
+    let likes: [Like];
+    let dislikes: [Dislike];
+    let comments: [Comment];
+}
+
+struct Like: Codable {
+    let id: String;
+    let user: User;
+}
+
+struct ApiProductData: Codable {
+    let availability: String;
+    let imageURL: String;
+    let description: String;
+    let sku: String;
+    let updatedAt: String;
+    
+    enum CodingKeys: String, CodingKey {
+        case availability
+        case imageURL = "image"
+        case description
+        case sku
+        case updatedAt
+    }
+}
+
+struct Dislike: Codable {
+    let id: String;
+    let user: User;
+}
+
+struct Comment: Codable {
+    let id: String;
+    let createdAt: String;
+    let text: String;
+    let user: User
+}
+
+struct User: Codable {
+    let id: String?;
+    let name: String;
+    let likes: [UserLikes]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        
+        if(container.contains(.id)) {
+            self.id = try container.decode(String.self, forKey: .id)
+        }
+        else {
+            self.id = nil;
+        }
+        
+        if(container.contains(.likes)) {
+            self.likes = try container.decode([UserLikes].self, forKey: .likes)
+        }
+        else {
+            self.likes = []
+        }
+    }
+}
+
+struct UserLikes: Codable {
+    
+}
