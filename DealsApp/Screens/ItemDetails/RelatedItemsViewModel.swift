d06d598ce2a20a88db234a554ea890235bccc36e
Cameron Robinson <cameron@ckrobinson.net>
2023-07-07 18:32:41 -0700

    Added related item fetch function.

--- DealsApp/Screens/ItemDetails/RelatedItemsViewModel.swift
+++ DealsApp/Screens/ItemDetails/RelatedItemsViewModel.swift
@@ -10,10 +10,39 @@ import Foundation
 class RelatedItemsViewModel: ObservableObject {
     
     @Published var relatedItems: [DealData] = []
-    let dealService: DealsService = DealsService();
+    private let dealService: DealsService = DealsService();
     
     func getRelatedItemsFromParent(_ parentDeal: DealData) {
         
+        var relatedItems: Set<String> = []
+        for like in parentDeal.rawData.likes {
+            
+            guard let userLikes = like.user.likes else {
+                continue;
+            }
+            
+            for like in userLikes {
+                relatedItems.insert(like.likedDeal.dealID)
+            }
+        }
+        relatedItems.remove(parentDeal.id)
         
+        for relatedItem in relatedItems {
+            
+            do {
+                if let item = try dealService.fetchDealByID(relatedItem) {
+                    
+                    self.relatedItems.append(DealData(rawData: item))
+                }
+            }
+            catch {
+                if let error = error as? APIError {
+                    print(error.description);
+                }
+                else {
+                    print(error.localizedDescription);
+                }
+            }
+        }
     }
 }
